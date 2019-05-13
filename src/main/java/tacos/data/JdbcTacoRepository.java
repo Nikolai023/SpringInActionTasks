857a06f11aaee72094cb027fa50ccfdab57b900f
AlexManson <nn_gerasimenko@mail.ru>
2019-05-13 21:34:22 +0700

    3.1 fixed. Changed prepared statement factory

--- src/main/java/tacos/data/JdbcTacoRepository.java
+++ src/main/java/tacos/data/JdbcTacoRepository.java
@@ -36,13 +36,18 @@ public class JdbcTacoRepository implements TacoRepository {
 
     private long saveTacoInfo(Taco taco) {
         taco.setCreatedAt(new Date());
-        PreparedStatementCreator psc = new PreparedStatementCreatorFactory(
+        PreparedStatementCreatorFactory pscFactory = new PreparedStatementCreatorFactory(
                 "insert into Taco (name, createdAt) values (?, ?)",
                 Types.VARCHAR, Types.TIMESTAMP
-        ).newPreparedStatementCreator(
+        );
+        pscFactory.setReturnGeneratedKeys(true);
+
+        PreparedStatementCreator psc = pscFactory.newPreparedStatementCreator(
                 Arrays.asList(
                         taco.getName(),
-                        new Timestamp(taco.getCreatedAt().getTime())));
+                        new Timestamp(taco.getCreatedAt().getTime())
+                )
+        );
 
         KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbc.update(psc, keyHolder);
