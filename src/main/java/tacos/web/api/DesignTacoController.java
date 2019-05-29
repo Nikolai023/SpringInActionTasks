fa7982aff490b646c8ff9486a1a7a50ad821c719
NGerasimenko <nn_gerasimenko@mail.ru>
2019-05-29 13:06:08 +0700

    6.2 created rest controller, added hyperlinks

---
+++ src/main/java/tacos/web/api/DesignTacoController.java
@@ -0,0 +1,48 @@
+package tacos.web.api;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tacos.Taco;
+import tacos.data.TacoRepository;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/design",
+        produces = "application/json")
+@CrossOrigin(origins = "*")
+public class DesignTacoController {
+    private TacoRepository tacoRepo;
+
+    private EntityLinks entityLinks;
+
+    public DesignTacoController(TacoRepository tacoRepo, EntityLinks entityLinks) {
+        this.tacoRepo = tacoRepo;
+        this.entityLinks = entityLinks;
+    }
+
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest page = PageRequest.of(
+                0, 12, Sort.by("createdAt").descending());
+        return tacoRepo.findAll(page).getContent();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> optionalTaco = tacoRepo.findById(id);
+        return optionalTaco
+                .map(taco -> new ResponseEntity<>(taco, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepo.save(taco);
+    }
+}
